69d4c017ab4042eca6e718dba1468e31cb029866
Rojin <rojindhami98@gmail.com>
2026-02-05 02:03:51 +0545

    Ceated the views.py of tasks app

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Task
 
-# Create your views here.
+@login_required
+def task_list(request):
+    tasks = Task.objects.filter(user=request.user)
+    if request.method == "POST":
+        new_task = request.POST.get('title')
+        if new_task:
+            Task.objects.create(user=request.user, title=new_task)
+        return redirect('task_list')
+    return render(request, 'tasks/list.html', {'tasks': tasks})
+
+@login_required
+def delete_task(request, pk):
+    Task.objects.get(id=pk, user=request.user).delete()
+    return redirect('task_list')
