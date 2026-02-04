5477646bbc831df3e4601a928deeb0f8fb3ef493
Rojin <rojindhami98@gmail.com>
2026-02-05 02:21:18 +0545

    Added templates and auth features with some styling

--- tasks/models.py
+++ tasks/models.py
@@ -5,7 +5,7 @@ class Task(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     completed = models.BooleanField(default=False)
-    created = models.DateTimeField(auto_auto_now_add=True)
+    created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
