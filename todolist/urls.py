cdb527ae7cdaea0e6f65de344644bcdb3fd0d5fc
Rojin <rojindhami98@gmail.com>
2026-02-05 02:05:51 +0545

    Managed the routing

--- todolist/urls.py
+++ todolist/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from tasks import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # Auth
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
+    # App
+    path('', views.task_list, name='task_list'),
+    path('delete/<int:pk>/', views.delete_task, name='delete_task'),
 ]
